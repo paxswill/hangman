9f7362af04bf4fa5f146613092d484ea02f50197
Will Ross <paxswill@paxswill.com>
2018-04-26 14:09:46 -0400

    Implement correct guesses

--- hangman.py
+++ hangman.py
@@ -29,7 +29,7 @@ class Game(object):
 
     def reset(self):
         self.word = random.choice(self.word_list)
-        self.wrong_guesses = 0
+        self._wrong_guesses = set()
         self.letters = list([None for _ in range(len(self.word))])
         self.state = GameState.INPROGRESS
 
@@ -37,16 +37,27 @@ class Game(object):
     def finished(self):
         return self.state != GameState.INPROGRESS
 
+    @property
+    def wrong_guesses(self):
+        return len(self._wrong_guesses)
+
     def guess(self, guesses):
         if self.finished:
             raise GameFinished()
         # Use lowercase for normalization
         lower_word = self.word.lower()
-        for guess in guesses:
-            if guess.lower() in lower_word:
-                pass
-            else:
-                self.wrong_guesses += 1
+        for guess in [g.lower() for g in guesses]:
+            if guess in lower_word:
+                # Doing this mirroring of the words to make sure the
+                # capitalization of the original word is preserved while
+                # guessing.
+                for index, letter in enumerate(lower_word):
+                    if letter == guess:
+                        self.letters[index] = self.word[index]
+                if None not in self.letters:
+                    self.state = GameState.WON
+            elif guess not in self._wrong_guesses:
+                self._wrong_guesses.add(guess)
                 if self.wrong_guesses >= self.num_guesses:
                     self.state = GameState.LOST
                     break
